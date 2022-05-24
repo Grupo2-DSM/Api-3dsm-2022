5f7b658c477f1a1036332232238e5dccd08b162a
Ana Carolina dos Santos Rocha <78958795+annakks@users.noreply.github.com>
2022-05-24 08:27:40 -0300

    deletado pasta .com

--- goodticket/back-end/src/main/java/com/example/banco/demo/repositorios/UsuarioRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.banco.demo.repositorios;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.banco.demo.entidades.Usuario;
-
-public interface UsuarioRepositorio extends MongoRepository<Usuario, String>{
-	
-}
